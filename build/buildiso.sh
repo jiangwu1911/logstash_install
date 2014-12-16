9d461166c87cf9bc864993607e1f3b49f655dd06
wujiang <jiangwu1911@hotmail.com>
2014-12-16 10:31:56 +0800

    Init version

---
+++ build/buildiso.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+ISODIR=../iso
+
+#0: Prepare files
+# mkdir -p ../iso
+# cp ../centos7/.discinfo ../iso
+# cp ../centos7/.treeinfo ../iso
+# cp -r ../centos7/Packages ../iso
+# cp -r ../centos7/images ../iso
+# cp -r ../centos7/isolinux ../iso
+# cp -r ../centos7/LiveOS ../iso
+
+# OS packages
+rm -rf $ISODIR/repodata/
+/bin/cp comps.xml $ISODIR
+createrepo -g comps.xml $ISODIR
+
+# logstash packages
+createrepo $ISODIR/software
+rm -rf $ISODIR/software/conf
+cp -r conf $ISODIR/software/
+
+mkdir -p $ISODIR/ks
+/bin/cp ks.cfg $ISODIR/ks
+/bin/cp isolinux.cfg $ISODIR/isolinux
+
+mkisofs -o logstash.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -V "logstash_install" -v -T $ISODIR
