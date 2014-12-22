b57c70aa93d14a44456d386b38436f7b198a95be
wujiang <jiangwu1911@hotmail.com>
2014-12-22 18:27:32 +0800

    let logstash run as root

--- fuel/install_logstash.sh
+++ fuel/install_logstash.sh
@@ -10,7 +10,11 @@ ssh $1 <<EOF
 yum install -y java-1.6.0-openjdk
 rpm -ivh fuel/logstash-1.4.2-1_2c0f5a1.noarch.rpm
 rpm -ivh fuel/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm
-/bin/cp fuel/logstash.conf /etc/logstash/conf.d
+/bin/cp fuel/agent.conf /etc/logstash/conf.d
+
+sed -i "s/LS_USER=.*/LS_USER=root/" /etc/init.d/logstash
+sed -i "s/LS_GROUP=.*/LS_GROUP=root/" /etc/init.d/logstash
+
 chkconfig logstash on
 service logstash start
 EOF
